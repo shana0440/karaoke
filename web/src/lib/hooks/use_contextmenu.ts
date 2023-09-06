49fab37797cea5d9465ea1eb8715e202887b4796
kwguo <shana0440@gmail.com>
2023-09-06 13:47:20 +0800

    remove unused context menu

--- web/src/lib/hooks/use_contextmenu.ts
+++
@@ -1,6 +0,0 @@
-import type { ContextMenuContext } from "$lib/components/context_menu.svelte";
-import { getContext } from "svelte";
-
-export const useContextMenu = (): ContextMenuContext => {
-  return getContext("contextmenu");
-};
