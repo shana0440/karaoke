240f3878340746e6a8bef6d6d5113c14288c16ca
kwguo <shana0440@gmail.com>
2023-09-07 12:21:50 +0800

    format

--- api/prepare_dataset.py
+++ api/prepare_dataset.py
@@ -15,23 +15,87 @@ def load_data_source():
 def splite_song_and_zatudan(stream_path, id, songs_range):
     songs = []
     zatudans = []
-    start = "00:00:00";
+    start = "00:00:00"
     for range in songs_range:
         start_time, end_time = range
-        zatudan = os.path.join(config.ZATSUDAN_DATASET_PATH, "zatudan_{}_{}_{}.wav".format(id, start, start_time))
-        subprocess.run(["ffmpeg", "-n", "-i", stream_path, "-ss", start, "-to", start_time, "-c", "copy", zatudan])
-        song = os.path.join(config.SONG_DATASET_PATH, "song_{}_{}_{}.wav".format(id, start_time, end_time))
-        subprocess.run(["ffmpeg", "-n", "-i", stream_path, "-ss", start_time, "-to", end_time, "-c", "copy", song])
+        zatudan = os.path.join(
+            config.ZATSUDAN_DATASET_PATH,
+            "zatudan_{}_{}_{}.wav".format(id, start, start_time),
+        )
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-n",
+                "-i",
+                stream_path,
+                "-ss",
+                start,
+                "-to",
+                start_time,
+                "-c",
+                "copy",
+                zatudan,
+            ]
+        )
+        song = os.path.join(
+            config.SONG_DATASET_PATH,
+            "song_{}_{}_{}.wav".format(id, start_time, end_time),
+        )
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-n",
+                "-i",
+                stream_path,
+                "-ss",
+                start_time,
+                "-to",
+                end_time,
+                "-c",
+                "copy",
+                song,
+            ]
+        )
         start = end_time
         songs.append(song)
         zatudans.append(zatudan)
-    
-    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", stream_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    result = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            stream_path,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
     total_duration = str(float(result.stdout))
-    zatudan = os.path.join(config.ZATSUDAN_DATASET_PATH, "zatudan_{}_{}_{}.wav".format(id, start, total_duration))
-    subprocess.run(["ffmpeg", "-n", "-i", stream_path, "-ss", start, "-to", total_duration, "-c", "copy", zatudan])
+    zatudan = os.path.join(
+        config.ZATSUDAN_DATASET_PATH,
+        "zatudan_{}_{}_{}.wav".format(id, start, total_duration),
+    )
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-n",
+            "-i",
+            stream_path,
+            "-ss",
+            start,
+            "-to",
+            total_duration,
+            "-c",
+            "copy",
+            zatudan,
+        ]
+    )
     zatudans.append(zatudan)
-    
+
     return zatudans, songs
 
 
@@ -43,16 +107,21 @@ def prepare_data(data, files, value):
             start_sample = config.NUM_SAMPLES_PER_SEGMENT * s
             finish_sample = start_sample + config.NUM_SAMPLES_PER_SEGMENT
 
-            mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample], sr=sr, n_fft=config.N_FFT, n_mfcc=config.N_MFCC, hop_length=config.HOP_LENGTH)
+            mfcc = librosa.feature.mfcc(
+                y=signal[start_sample:finish_sample],
+                sr=sr,
+                n_fft=config.N_FFT,
+                n_mfcc=config.N_MFCC,
+                hop_length=config.HOP_LENGTH,
+            )
             mfcc = mfcc.T
 
             if len(mfcc) == config.EXPECTED_NUM_MFCC_VECTORS_PER_SEGMENT:
                 data["mfcc"].append(mfcc.tolist())
-                data["labels"].append(value)    
+                data["labels"].append(value)
 
 
 if __name__ == "__main__":
-
     data = {
         "mapping": ["song", "zatsudan"],
         "mfcc": [],
@@ -60,16 +129,44 @@ if __name__ == "__main__":
     }
 
     source = load_data_source()
-    
+
     for i, stream in enumerate(source["streams"]):
-        id = utils.extract_youtube_id(stream["url"]);
-        name, out_name = ("{}/karaoke_{}".format(config.KARAOKE_DATASET_PATH, id), "{}/karaoke_{}_out".format(config.KARAOKE_DATASET_PATH, id))
-        subprocess.run(["yt-dlp", "-f", "wv", "-f", "ba", stream["url"], "-o", "{}.webm".format(name)])
-        subprocess.run(["ffmpeg", "-n", "-i", "{}.webm".format(name), "-acodec", "pcm_s16le", "-ar", "44100", "{}.wav".format(name)])
-        zatudans, songs = splite_song_and_zatudan("{}.wav".format(name), id, stream["songs"])
+        id = utils.extract_youtube_id(stream["url"])
+        name, out_name = (
+            "{}/karaoke_{}".format(config.KARAOKE_DATASET_PATH, id),
+            "{}/karaoke_{}_out".format(config.KARAOKE_DATASET_PATH, id),
+        )
+        subprocess.run(
+            [
+                "yt-dlp",
+                "-f",
+                "wv",
+                "-f",
+                "ba",
+                stream["url"],
+                "-o",
+                "{}.webm".format(name),
+            ]
+        )
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-n",
+                "-i",
+                "{}.webm".format(name),
+                "-acodec",
+                "pcm_s16le",
+                "-ar",
+                "44100",
+                "{}.wav".format(name),
+            ]
+        )
+        zatudans, songs = splite_song_and_zatudan(
+            "{}.wav".format(name), id, stream["songs"]
+        )
 
         prepare_data(data, songs, 0)
         prepare_data(data, zatudans, 1)
-        
+
     with open(config.JSON_PATH, "w") as fp:
         json.dump(data, fp)
--- api/train_cnn.py
+++ api/train_cnn.py
@@ -5,14 +5,15 @@ import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 import config
 
+
 def load_data(dataset_path):
-   with open(dataset_path, "r") as fp:
-    data = json.load(fp)
+    with open(dataset_path, "r") as fp:
+        data = json.load(fp)
 
-    inputs = np.array(data["mfcc"])
-    targets = np.array(data["labels"])
+        inputs = np.array(data["mfcc"])
+        targets = np.array(data["labels"])
 
-    return inputs, targets
+        return inputs, targets
 
 
 def prepare_datasets(test_size, validation_size):
@@ -20,10 +21,14 @@ def prepare_datasets(test_size, validation_size):
     inputs, targets = load_data(config.JSON_PATH)
 
     # create train/test split
-    x_train, x_test, y_train, y_test = train_test_split(inputs, targets, test_size=test_size)
+    x_train, x_test, y_train, y_test = train_test_split(
+        inputs, targets, test_size=test_size
+    )
 
     # create train/validation split
-    x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, test_size=validation_size)
+    x_train, x_validation, y_train, y_validation = train_test_split(
+        x_train, y_train, test_size=validation_size
+    )
 
     x_train = x_train[..., np.newaxis]
     x_validation = x_validation[..., np.newaxis]
@@ -36,27 +41,33 @@ def build_model(input_shape):
     model = keras.Sequential()
 
     # 1st conv layer, 32 filter(kernel) that size is 3x3
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(
+        keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape)
+    )
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding="same"))
     model.add(keras.layers.BatchNormalization())
 
     # 2nd conv layer
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(
+        keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape)
+    )
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding="same"))
     model.add(keras.layers.BatchNormalization())
 
     # 3rd conv layer
-    model.add(keras.layers.Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(
+        keras.layers.Conv2D(32, (2, 2), activation="relu", input_shape=input_shape)
+    )
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding="same"))
     model.add(keras.layers.BatchNormalization())
 
     # flatten the output and feed it into dense layer
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dense(64, activation="relu"))
     model.add(keras.layers.Dropout(0.5))
 
     # output layer, 2 neurons to represent is karaoke or zatsudan
-    model.add(keras.layers.Dense(2, activation='softmax'))
+    model.add(keras.layers.Dense(2, activation="softmax"))
 
     return model
 
@@ -84,7 +95,9 @@ def plot_history(history):
 
 if __name__ == "__main__":
     # create train, validation and test sets
-    x_train, x_validation, x_test, y_train, y_validation, y_test = prepare_datasets(0.1, 0.2)
+    x_train, x_validation, x_test, y_train, y_validation, y_test = prepare_datasets(
+        0.1, 0.2
+    )
 
     # build the CNN net
     input_shape = (x_train.shape[1], x_train.shape[2], x_train.shape[3])
@@ -92,12 +105,22 @@ if __name__ == "__main__":
 
     # compile the network
     optimizer = keras.optimizers.Adam(learning_rate=0.0001)
-    model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile(
+        optimizer=optimizer,
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
 
     model.summary()
 
     # train the CNN
-    history = model.fit(x_train, y_train, validation_data=(x_validation, y_validation), epochs=50, batch_size=32)
+    history = model.fit(
+        x_train,
+        y_train,
+        validation_data=(x_validation, y_validation),
+        epochs=50,
+        batch_size=32,
+    )
 
     # evaluate the CNN on the test set
     test_error, test_accuracy = model.evaluate(x_test, y_test, verbose=1)
@@ -105,6 +128,6 @@ if __name__ == "__main__":
 
     # save model
     model.save(config.MODEL_PATH)
-    
+
     # plot accuracy and error over the epochs
     plot_history(history)
