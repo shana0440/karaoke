4311b8c50a27d8a9f706fb81425e033dae8abe9b
kwguo <shana0440@gmail.com>
2023-09-09 22:53:28 +0800

    use melt ui implement slider

--- web/src/lib/components/slider.svelte
+++ web/src/lib/components/slider.svelte
@@ -1,45 +1,46 @@
 <script lang="ts">
-  import Thumb from "./thumb.svelte";
-  import { useDrag } from "$lib/hooks/use_drag";
+  import { createSlider, melt } from "@melt-ui/svelte";
+  import { writable } from "svelte/store";
 
   export let value: [number, number];
   export let min: number;
   export let max: number;
 
-  let container: HTMLDivElement;
+  let valuePipe = writable<number[]>(value);
 
-  $: windows = [(value[0] - min) / max, (value[1] - min) / max];
-
-  const moveWindow = (index: 0 | 1) => (delta: number) => {
-    const newValue =
-      value[index] + (delta / container.clientWidth) * (max - min);
-    value[index] = Math.min(Math.max(newValue, min), max);
-    value = [...value];
-  };
-
-  const sortValue = () => {
-    if (value[0] > value[1]) {
-      value = [value[1], value[0]];
-    }
-  };
-
-  const { moving, drag } = useDrag((delta) => {
-    moveWindow(0)(delta);
-    moveWindow(1)(delta);
+  const {
+    elements: { root, range, thumb },
+    options: { max: slideMax, min: slideMin },
+    states: { value: slideValue },
+  } = createSlider({
+    defaultValue: [20, 80],
+    step: 1,
+    value: valuePipe,
   });
 
-  $: range = windows.sort();
+  $: {
+    slideMax.set(max);
+    slideMin.set(min);
+  }
+
+  $: {
+    value = $slideValue as [number, number];
+  }
+
+  $: {
+    valuePipe.set(value);
+  }
 </script>
 
-<div bind:this={container} class="relative flex items-center py-2">
-  <div class="w-full h-2 border bg-gunmetal border-cod-gray" />
-  <button
-    on:mousedown={drag}
-    style={`left: ${range[0] * 100}%; right: ${100 - range[1] * 100}%`}
-    class={`absolute h-2 transition bg-purple-taupe hover:scale-y-150 ${
-      $moving ? "scale-y-150" : ""
-    }`}
-  />
-  <Thumb offset={windows[0]} onChange={moveWindow(0)} onDropped={sortValue} />
-  <Thumb offset={windows[1]} onChange={moveWindow(1)} onDropped={sortValue} />
-</div>
+<span use:melt={$root} class="relative flex h-[20px] items-center">
+  <span class="block h-2 w-full bg-gunmetal">
+    <span use:melt={$range} class="h-2 bg-purple-taupe" />
+  </span>
+
+  {#each $slideValue as _}
+    <span
+      use:melt={$thumb()}
+      class="block h-5 w-5 rounded-full bg-alice-blue focus:ring-4 focus:ring-cod-gray/40"
+    />
+  {/each}
+</span>
