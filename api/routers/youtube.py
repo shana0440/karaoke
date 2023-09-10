01536d57e0fbc2260c6968d972f632607ee6967d
kwguo <shana0440@gmail.com>
2023-09-10 17:32:33 +0800

    fix throw error when no comment

--- api/routers/youtube.py
+++ api/routers/youtube.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from services.youtube import get_video_most_like_comment
 
 router = APIRouter()
 
+
 @router.get("/videos/{video_id}/most-like-comment")
 async def get_most_like_comment(video_id: str):
     comment = get_video_most_like_comment(video_id)
+    if comment is None:
+        raise HTTPException(status_code=404, detail="comment not found")
     return comment.snippet.topLevelComment.snippet
